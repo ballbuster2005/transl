15b975ba63b23880a64f2adf168b249548e8a998
ballbuster2005 <61076179+ballbuster2005@users.noreply.github.com>
2020-05-20 15:24:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ExampleController from './components/Example';
-import PerederiyVAController from './components/PerederiyVA';
-import PetrovSDController from './components/PetrovSD/PetrovSDController'
-import ZolototrubovERController from './components/ZolototrubovER/ZolototrubovERController'
+import Transliteration from './component/Transliteration'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <PerederiyVAController/>
-
-        <PetrovSDController/>
-
-        <ZolototrubovERController/>
-
-        <ExampleController/>
-
-      </header>
+      <Transliteration></Transliteration>
     </div>
   );
 }
-
-// Add only top-level component to the App
 
 export default App;
